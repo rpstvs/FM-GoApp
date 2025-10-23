2cd4271af40380c926ee598bf2e8cf59b529785d
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-10-23 20:09:00 +0100

    adding a server

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/rpstvs/fm-goapp/internal/app"
+)
+
+func main() {
+	app, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("we Are running!")
+
+	server := &http.Server{
+		Addr:        ":8080",
+		IdleTimeout: 30 * Time.Second,
+	}
 }
