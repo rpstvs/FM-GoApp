3cab3d00abd8cbded8839226c1b54c92fabe359a
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-10-24 16:37:51 +0100

    health check on server

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/rpstvs/fm-goapp/internal/app"
 )
 
 func main() {
-	app, err := app.NewApplication()
 
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
 	app.Logger.Println("we Are running!")
-
+	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
-		Addr:        ":8080",
-		IdleTimeout: 30 * Time.Second,
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "OK\n")
+	w.WriteHeader(200)
+
 }
