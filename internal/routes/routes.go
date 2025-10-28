7a4ede6103d0aba8b44c5be31f1b218dbddbe59b
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-10-28 19:33:00 +0000

    changes on routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/rpstvs/fm-goapp/internal/app"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkById)
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+
+	return r
+}
